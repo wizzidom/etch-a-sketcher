0e1068d71517258396065de00c4ea73088e4af4f
wizzidom <majolawisdom@gmail.com>
2024-07-13 01:30:52 +0200

    Added the grid and hover effect

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 const container = document.querySelector('.container');
-console.log(container);
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
+  
+
+var grids = [];
+
+for (let i = 0; i < 16; i++) {
+    grids[i] = document.createElement('div');
+    grids[i].classList.add('box');
+    grids[i].style.width = '50px';
+    grids[i].style.height = '50px';
+    grids[i].style.border = '1px solid red';
+    container.appendChild(grids[i]);
+    
+}
+var box = document.querySelector('box');
+box.addEventListener("mouseover", ()=>{
+    box.style.backgroundColor = randomColor()
+})
+console.log(grids)
